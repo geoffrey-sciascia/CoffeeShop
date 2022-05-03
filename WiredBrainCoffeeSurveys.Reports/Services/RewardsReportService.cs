af1e04f329459c286bab823ca536368588ccacb7
Geoffrey-Sciascia <gwsciascia@gmail.com>
2022-05-03 22:43:02 +1200

    refactor survey result data access to a service

--- WiredBrainCoffeeSurveys.Reports/Services/RewardsReportService.cs
+++ WiredBrainCoffeeSurveys.Reports/Services/RewardsReportService.cs
@@ -1,10 +1,36 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace WiredBrainCoffeeSurveys.Reports.Services
 {
-    public class RewardsReportService
+    public static class RewardsReportService
     {
-        public RewardsReportService()
+        // Add JSON variable to methods as paremters and an alias to be used within each method
+        public static void GenerateWinnerEmails(SurveyResults results)
         {
+            var selectedEmails = new List<string>();
+            int counter = 0;
+
+            Console.WriteLine(Environment.NewLine + "Selected Winners Output:");
+            // Replace Q1Results with JSON variable alias each time it is called in each method
+            while (selectedEmails.Count < 2 && counter < results.Responses.Count)
+            {
+                var currentItem = results.Responses[counter];
+
+                if (currentItem.FavoriteProduct == "Cappucino")
+                {
+                    selectedEmails.Add(currentItem.EmailAddress);
+                    Console.WriteLine(currentItem.EmailAddress);
+                }
+
+                counter++;
+            }
+
+            File.WriteAllLines("WinnersReport.csv", selectedEmails);
         }
     }
 }
--- WiredBrainCoffeeSurveys.Reports/Services/TasksReportService.cs
+++ WiredBrainCoffeeSurveys.Reports/Services/TasksReportService.cs
@@ -1,10 +1,53 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace WiredBrainCoffeeSurveys.Reports.Services
 {
-    public class TasksReportService
+    public static class TasksReportService
     {
-        public TasksReportService()
+        public static void GenerateTasksReport(SurveyResults results)
         {
+            var tasks = new List<string>();
+
+            double responseRate = results.NumberResponded / results.NumberSurveyed;
+            double overallScore = (results.ServiceScore + results.CoffeeScore + results.FoodScore + results.PriceScore) / 4;
+
+            if (results.CoffeeScore < results.FoodScore)
+            {
+                tasks.Add("Investigate coffee recipes and ingredients.");
+            }
+
+            // Change traditional if/else into a ternary statement
+            tasks.Add(overallScore > 8.0 ? "Work with leadership to reward staff" : "Work with employees for improvement ideas.");
+
+            // swap if else logic for switch statement using when keyword, more elegant but less readable than if else statement.
+            tasks.Add(responseRate switch
+            {
+                var rate when rate < .33 => "Research options to improve response rate.",
+                var rate when rate > .33 && rate < .66 => "Reward participants with free coffee coupon.",
+                var rate when rate > .66 => "Reward participants with discount coffee coupon."
+            });
+
+            // Improved switch statement with fat arrow (lambda operator) syntax, finished of with underscore (discard operator) syntax
+            tasks.Add(results.AreaToImprove switch
+            {
+                "RewardPrograms" => "Revisit the rewards deals.",
+                "Cleanliness" => "Contact the cleaning vendor.",
+                "MobileApp" => "Contact the consulting firm about the app.",
+                _ => "Investigate individual comments for ideas."
+            });
+
+            Console.WriteLine(Environment.NewLine + "Tasks Output:");
+            foreach (var task in tasks)
+            {
+                Console.WriteLine(task);
+            }
+
+            File.WriteAllLines("TasksReport.csv", tasks);
         }
     }
 }
