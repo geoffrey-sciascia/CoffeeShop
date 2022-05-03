129f4e9974969a9bd8eb6c8b85d7efb9f611048e
Geoffrey-Sciascia <gwsciascia@gmail.com>
2022-05-03 19:40:14 +1200

    working coffee shop admin app

---
+++ WiredBrainCoffeeSurveys.Reports/Program.cs
@@ -0,0 +1,150 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+// Add JSON reader in above imports
+
+namespace WiredBrainCoffeeSurveys.Reports
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Add boolean variable to allow a quit option for the user to quit the program
+            bool quitApp = false;
+
+            // Do While statement to allow the user to continue to read different report data in the same session until quit is typed
+            do
+            {
+                Console.WriteLine("Please specify a report to run (rewards, comments, tasks, quit):");
+                var selectedReport = Console.ReadLine();
+
+                // Add second user choice to specify which set of JSON data to read
+                Console.WriteLine("Please specify which quarter of data: (q1, q2)");
+                var selectedData = Console.ReadLine();
+
+                // Add new variable to hold the C# compiled JSON data
+                var surveyResults = JsonConvert.DeserializeObject<SurveyResults>
+                    (File.ReadAllText($"data/{selectedData}.json"));
+
+                // Switch statement to allow the choices of the user to generate the corresponding report data
+                switch (selectedReport)
+                {
+                    case "rewards":
+                        // Add JSON variable as a paremter to the methods 
+                        GenerateWinnerEmails(surveyResults);
+                        break;
+                    case "tasks":
+                        GenerateTasksReport(surveyResults);
+                        break;
+                    case "comments":
+                        GenerateCommentsReport(surveyResults);
+                        break;
+                    case "quit":
+                        quitApp = true;
+                        break;
+                    default:
+                        Console.WriteLine("Sorry, that's not a valid option.");
+                        break;
+                }
+
+                Console.WriteLine();
+            }
+            while (!quitApp);
+
+        }
+
+        // Add JSON variable to methods as paremters and an alias to be used within each method
+        public static void GenerateWinnerEmails(SurveyResults results)
+        {
+            var selectedEmails = new List<string>();
+            int counter = 0;
+
+            Console.WriteLine(Environment.NewLine + "Selected Winners Output:");
+            // Replace Q1Results with JSON variable alias each time it is called in each method
+            while (selectedEmails.Count < 2 && counter < results.Responses.Count)
+            {
+                var currentItem = results.Responses[counter];
+
+                if (currentItem.FavoriteProduct == results.FavoriteProduct)
+                {
+                    selectedEmails.Add(currentItem.EmailAddress);
+                    Console.WriteLine(currentItem.EmailAddress);
+                }
+
+                counter++;
+            }
+
+            File.WriteAllLines("WinnersReport.csv", selectedEmails);
+        }
+
+        public static void GenerateCommentsReport(SurveyResults results)
+        {
+            var comments = new List<string>();
+
+            Console.WriteLine(Environment.NewLine + "Comments Output:");
+            for (var i = 0; i < results.Responses.Count; i++)
+            {
+                var currentResponse = results.Responses[i];
+
+                if (currentResponse.WouldRecommend < 7.0)
+                {
+                    Console.WriteLine(currentResponse.Comments);
+                    comments.Add(currentResponse.Comments);
+                }
+            }
+
+            foreach (var response in results.Responses)
+            {
+                if (response.AreaToImprove == results.AreaToImprove)
+                {
+                    Console.WriteLine(response.Comments);
+                    comments.Add(response.Comments);
+                }
+            }
+
+            File.WriteAllLines("CommentsReport.csv", comments);
+        }
+
+        public static void GenerateTasksReport(SurveyResults results)
+        {
+            var tasks = new List<string>();
+
+            double responseRate = results.NumberResponded / results.NumberSurveyed;
+            double overallScore = (results.ServiceScore + results.CoffeeScore + results.FoodScore + results.PriceScore) / 4;
+
+            if (results.CoffeeScore < results.FoodScore)
+            {
+                tasks.Add("Investigate coffee recipes and ingredients.");
+            }
+
+            // Change traditional if/else into a ternary statement
+            tasks.Add(overallScore > 8.0 ? "Work with leadership to reward staff" : "Work with employees for improvement ideas.");
+
+            // swap if else logic for switch statement using when keyword, more elegant but less readable than if else statement.
+            tasks.Add(responseRate switch
+            {
+                var rate when rate < .33 => "Research options to improve response rate.",
+                var rate when rate > .33 && rate < .66 => "Reward participants with free coffee coupon.",
+                var rate when rate > .66 => "Reward participants with discount coffee coupon."
+            });
+
+            // Improved switch statement with fat arrow (lambda operator) syntax, finished of with underscore (discard operator) syntax
+            tasks.Add(results.AreaToImprove switch
+            {
+                "RewardPrograms" => "Revisit the rewards deals.",
+                "Cleanliness" => "Contact the cleaning vendor.",
+                "MobileApp" => "Contact the consulting firm about the app.",
+                _ => "Investigate individual comments for ideas."
+            });
+
+            Console.WriteLine(Environment.NewLine + "Tasks Output:");
+            foreach(var task in tasks)
+            {
+                Console.WriteLine(task);
+            }
+
+            File.WriteAllLines("TasksReport.csv", tasks);
+        }
+    }
+}
---
+++ WiredBrainCoffeeSurveys.Reports/SurveyResults.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WiredBrainCoffeeSurveys.Reports
+{
+    public class SurveyResults
+    {
+        // Aggregate ratings, Get rid of hardcoded data, and static typing.
+        public double ServiceScore { get; set; }
+
+        public double CoffeeScore { get; set; }
+
+        public double PriceScore { get; set; }
+
+        public double FoodScore { get; set; }
+
+        public double WouldRecommend { get; set; }
+
+        public string FavoriteProduct { get; set; }
+
+        public string LeastFavoriteProduct { get; set; }
+
+        public string AreaToImprove { get; set; }
+
+        // Aggregate counts, Get rid of hardcoded data, and static typing.
+        public double NumberSurveyed { get; set; }
+
+        public double NumberResponded { get; set; }
+
+        public double NumberRewardsMembers { get; set; }
+
+        // Individual survey, responses Get rid of hardcoded data, and static typing.
+        public List<SurveyResponse> Responses { get; set; }
+    }
+}
